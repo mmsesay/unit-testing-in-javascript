b8477f20ddfb980a17f38135072065dc5e29be60
mmsesay <maej_01@hotmail.com>
2021-09-28 16:25:27 +0000

    Refactor: function to check for longer strings Add: test for the that function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-const stringLength = (string) => [...string].length;
+const _ = require('lodash');
+
+const stringLength = (string) => {
+  let result;
+  if (_.inRange(string.length, 1, 11)) {
+    result = string.length;
+  } else {
+    throw new Error('The string should be between 1 and 10 characters long');
+  }
+  return result;
+};
 
 module.exports = stringLength;
